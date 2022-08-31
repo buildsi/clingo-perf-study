c896b226cfae4d1fc33c65662f9f09991b24699d
Todd Gamblin <tgamblin@llnl.gov>
2022-08-31 03:18:31 -0400

    Redoing figs with Helvetica

--- container/analyze_deps_vs_time.py
+++ container/analyze_deps_vs_time.py
@@ -6,6 +6,15 @@ import glob
 import re
 import matplotlib.pyplot as plt
 
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+
+labelsize=20
+
+import matplotlib
+matplotlib.rc('xtick', labelsize=labelsize)
+matplotlib.rc('ytick', labelsize=labelsize)
+
 DESCRIPTION = """Produce a scatter plot of the "Total execution time" vs. "Number of possible dependencies" """
 
 SOLUTION_PHASES = 'setup', 'load', 'ground', 'solve'
@@ -13,9 +22,19 @@ SOLUTION_PHASES = 'setup', 'load', 'ground', 'solve'
 # Basic command line options
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 parser.add_argument('csvfile', help='CSV file with timing data')
+parser.add_argument('variable', help='variable (column) to plot')
+parser.add_argument('outfile', help='output file name')
 args = parser.parse_args()
 
-df = pd.read_csv(args.csvfile, header=None, names=['pkg', 'cfg', 'iter', 'setup', 'load', 'ground', 'solve', 'total', 'dep_len'])
+df = pd.read_csv(
+    args.csvfile,
+    header=None,
+    names=['pkg', 'cfg', 'iter', 'setup', 'load', 'ground', 'solve', 'total', 'dep_len'],
+    converters={'cfg': str.strip}
+)
+
+df = df[df.cfg == "tweety"]
+
 
 df_full = df
 print(df_full.head())
@@ -28,11 +47,10 @@ deps = []
 
 df_deps = df_full
 
-fig, axs = plt.subplots(figsize=(6, 6), dpi=150)  
+fig, axs = plt.subplots(figsize=(6, 6), dpi=150)
 
-df.plot.scatter(x="dep_len", y="total", ax=axs)
-axs.set_xlabel('Number of possible dependencies', fontsize=20)
-axs.set_ylabel('Total time [s]', fontsize=20)
-fig.savefig("total_time_vs_deps.png")
+df.plot.scatter(x="dep_len", y=args.variable, ax=axs)
+axs.set_xlabel('Number of dependencies', fontsize=labelsize)
+axs.set_ylabel('Time [s]', fontsize=labelsize)
 
-
+fig.savefig(args.outfile)

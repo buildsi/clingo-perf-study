26104047b2260ec5c6c9139d09572a444acb324c
Massimiliano Culpo <massimiliano.culpo@gmail.com>
2022-08-31 11:10:06 +0200

    Add cdf_quartz_total_fig.pdf

--- container/analyze_times_cdf.py
+++ container/analyze_times_cdf.py
@@ -6,6 +6,10 @@ import glob
 import re
 import matplotlib.pyplot as plt
 
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
+
 DESCRIPTION = """Produce a CDF plot of Package number vs. Total execution time"""
 
 SOLUTION_PHASES = 'setup', 'load', 'ground', 'solve'
@@ -13,9 +17,18 @@ SOLUTION_PHASES = 'setup', 'load', 'ground', 'solve'
 # Basic command line options
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 parser.add_argument('csvfile', help='CSV file with timing data')
+parser.add_argument('outfile', help='output file name')
 args = parser.parse_args()
 
-df = pd.read_csv(args.csvfile, header=None, names=['pkg', 'cfg', 'iter', 'setup', 'load', 'ground', 'solve', 'total', 'dep_len'])
+df = pd.read_csv(
+    args.csvfile,
+    header=None,
+    names=['pkg', 'cfg', 'iter', 'setup', 'load', 'ground', 'solve', 'total', 'dep_len'],
+    converters={'cfg': str.strip}
+)
+
+df = df[df.cfg != "many"]
+df = df[df.iter == 0]
 
 print(df.head())
 
@@ -24,14 +37,14 @@ pkg_ls = list(sorted(set(df['pkg'])))
 
 fig, axs = plt.subplots(figsize=(6, 6), dpi=150)  
 
-for cfg in cfg_ls:
+for cfg, color in [('tweety', "tab:red"), ('trendy', "tab:blue"), ('handy', "tab:purple")]:
     df_by_config = df[df['cfg'] == cfg]
     times = df_by_config['total']
-    times.hist(cumulative=True, density=1, bins=100, ax=axs, label=cfg, histtype='step')
+    times.hist(cumulative=True, density=False, bins=500, ax=axs, label=cfg, histtype='step', color=color)
 
-axs.set_xlabel('Total Time [sec.]', fontsize=20)
-axs.set_ylabel('Percentage of package', fontsize=20)
-axs.legend(loc='upper left')
-fig.savefig("total_time_cdf.png")
+axs.set_xlabel('Time [s]', fontsize=20)
+axs.set_ylabel('Package count', fontsize=20)
+axs.legend(loc='lower right')
 
-
+fig.tight_layout()
+fig.savefig(args.outfile)
